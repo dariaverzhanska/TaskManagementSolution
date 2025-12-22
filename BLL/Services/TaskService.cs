b45103b0966a9c985ac69dc4411522249bd1fa04
dariaverzhanska <dariaverzhanska@lll.kpi.ua>
2025-12-22 22:25:57 +0100

    Додано TaskService у BLL.Services

--- BLL/Services/TaskService.cs
+++ BLL/Services/TaskService.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using DAL.Entities;
+using DAL.Repositories;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace BLL.Services
 {
-    internal class Class1
+    public class TaskService
     {
+        private TaskRepository repository = new TaskRepository();
+
+        public void CreateTask(Task task) => repository.AddTask(task);
+
+        public void CompleteTask(int id)
+        {
+            var task = repository.GetAllTasks().FirstOrDefault(t => t.Id == id);
+            if (task != null) task.Status = "Completed";
+        }
+
+        public List<Task> GetTasksByEmployee(int employeeId)
+            => repository.GetAllTasks().Where(t => t.AssignedTo == employeeId).ToList();
     }
 }
