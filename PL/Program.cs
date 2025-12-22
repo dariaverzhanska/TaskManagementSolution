bf4db58fb6c1d1744bde1ba17b4b648b2d55abd9
dariaverzhanska <dariaverzhanska@lll.kpi.ua>
2025-12-22 22:27:11 +0100

    Додано Program.cs у PL

--- PL/Program.cs
+++ PL/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using BLL.Services;
+using DAL.Entities;
+
+namespace PL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskService taskService = new TaskService();
+            EmployeeService employeeService = new EmployeeService();
+
+            Employee emp = new Employee { Id = 1, Name = "Дар'я", Position = "Менеджер" };
+            employeeService.AddEmployee(emp);
+
+            Task task = new Task { Id = 1, Title = "Зробити звіт", AssignedTo = 1, Status = "Нова" };
+            taskService.CreateTask(task);
+
+            Console.WriteLine("Задачу успішно додано!");
+        }
+    }
+}
