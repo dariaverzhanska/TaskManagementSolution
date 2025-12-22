567f8a6d95819eadbf8cbe700fb269c24cef3a09
dariaverzhanska <dariaverzhanska@lll.kpi.ua>
2025-12-22 22:24:45 +0100

    Додано TaskRepository у DAL.Repositories

--- DAL/Repositories/TaskRepository.cs
+++ DAL/Repositories/TaskRepository.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using DAL.Entities;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace DAL.Repositories
 {
-    internal class Class1
+    public class TaskRepository
     {
+        private List<Task> tasks = new List<Task>();
+
+        public List<Task> GetAllTasks() => tasks;
+
+        public void AddTask(Task task) => tasks.Add(task);
+
+        public void UpdateTask(Task task)
+        {
+            var existing = tasks.FirstOrDefault(t => t.Id == task.Id);
+            if (existing != null)
+            {
+                existing.Title = task.Title;
+                existing.Description = task.Description;
+                existing.AssignedTo = task.AssignedTo;
+                existing.Status = task.Status;
+            }
+        }
+
+        public void DeleteTask(int id)
+        {
+            var task = tasks.FirstOrDefault(t => t.Id == id);
+            if (task != null) tasks.Remove(task);
+        }
     }
 }
