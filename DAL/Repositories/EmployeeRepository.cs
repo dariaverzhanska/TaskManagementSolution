51da543ae763156f7c7b0feb39d959aea841802a
dariaverzhanska <dariaverzhanska@lll.kpi.ua>
2025-12-22 22:25:13 +0100

    Додано EmployeeRepository у DAL.Repositories

--- DAL/Repositories/EmployeeRepository.cs
+++ DAL/Repositories/EmployeeRepository.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using DAL.Entities;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace DAL.Repositories
 {
-    internal class Class1
+    public class EmployeeRepository
     {
+        private List<Employee> employees = new List<Employee>();
+
+        public List<Employee> GetAllEmployees() => employees;
+
+        public void AddEmployee(Employee employee) => employees.Add(employee);
+
+        public void UpdateEmployee(Employee employee)
+        {
+            var existing = employees.FirstOrDefault(e => e.Id == employee.Id);
+            if (existing != null)
+            {
+                existing.Name = employee.Name;
+                existing.Position = employee.Position;
+            }
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var employee = employees.FirstOrDefault(e => e.Id == id);
+            if (employee != null) employees.Remove(employee);
+        }
     }
 }
