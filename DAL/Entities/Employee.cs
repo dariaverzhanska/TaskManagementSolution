afe7a2e784851c6f7f309d3450e60614b180153d
dariaverzhanska <dariaverzhanska@lll.kpi.ua>
2025-12-22 22:24:10 +0100

    Додано клас Employee у DAL.Entities

--- DAL/Entities/Employee.cs
+++ DAL/Entities/Employee.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace DAL.Entities
+﻿namespace DAL.Entities
 {
-    internal class Class1
+    public class Employee
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
     }
 }
